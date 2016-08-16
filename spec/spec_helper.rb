08db75bd9e0b91d59ee7f68ca2b81fed5400af36
Lynn Cyrin <firemagelynn@gmail.com>
2016-08-16 04:33:36 -0700

    more coveralls setup

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,8 @@
-require "simplecov"
 require "coveralls"
 require "lita-timecard"
 require "lita/rspec"
+
+Coveralls.wear!
 
 # A compatibility mode is provided for older plugins upgrading from Lita 3. Since this plugin
 # was generated with Lita 4, the compatibility mode should be left disabled.
@@ -10,9 +11,3 @@ Lita.version_3_compatibility_mode = false
 RSpec.configure do |config|
   config.run_all_when_everything_filtered = true
 end
-
-SimpleCov.formatters = [
-  SimpleCov::Formatter::HTMLFormatter,
-  Coveralls::SimpleCov::Formatter
-]
-SimpleCov.start { add_filter "/spec/" }
